410e1775f7c3f4d0c9eccc2163eab47184ec9c42
Sivarat Malapet <smalapet@gmail.com>
2015-10-03 16:29:34 +0700

    Add source code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Java-Face-Detection-OpenCV
-Basic face detection using Open CV and implementation in Java
+Basic face detection using Open CV implemented in Java
 
 Copyrights 2015 Sivarat Malapet all rights reserved.
---
+++ src/testOpenCV/FaceDetectorTest.java
@@ -0,0 +1,55 @@
+package testOpenCV;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+//
+// Detects faces in an image, draws boxes around them, and writes the results
+// to "faceDetection.png".
+//
+class DetectFaceDemo {
+  public void run(String picName) {
+	  
+    System.out.println("\nRunning DetectFaceDemo");
+
+    // Create a face detector from the cascade file in the resources
+    // directory.
+    CascadeClassifier faceDetector = new CascadeClassifier("cascades/haarcascade_frontalface_default.xml");
+    Mat image = Highgui.imread(picName);
+
+    // Detect faces in the image.
+    // MatOfRect is a special container class for Rect.
+    MatOfRect faceDetections = new MatOfRect();
+    faceDetector.detectMultiScale(image, faceDetections);
+
+    System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+    // Draw a bounding box around each face.
+    for (Rect rect : faceDetections.toArray()) {
+        Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+    }
+
+    // Save the visualized detection.
+    String filename = "faces/faceDetection.jpg";
+    System.out.println(String.format("Writing %s", filename));
+    Highgui.imwrite(filename, image);
+  }
+}
+
+public class FaceDetectorTest {
+  public static void main(String[] args) {
+    System.out.println("FaceDetectorTest started..");
+
+    // Load the native library.
+    //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    System.load("/home/sivarat/opencv-2.4.11/build/lib/libopencv_java2411.so");
+    new DetectFaceDemo().run("faces/lena.jpg");
+  }
+}
